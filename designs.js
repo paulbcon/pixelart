63fd959344a6112df66af361eb9aab4e10faaa56
Paul <paulbcon@hotmail.com>
2022-07-06 21:23:41 -0400

    docs: Add comments to explained each function

--- designs.js
+++ designs.js
@@ -1,77 +1,88 @@
+/*
+ * Project Name: Pixel Art Maker Project
+ * HTML and CSS credited to by Udacity
+ */
 
 /*
-* Make a grid based on user's specified width and height
-*/
+ * Make a grid based on user's specified width and height
+ */
 const makeGrid = () => {
-    // Your code goes here!
+  // Your code goes here!
 
-    let gridHeight = document.getElementById('inputHeight').value;
-    let gridWidth = document.getElementById('inputWidth').value;
+  let gridHeight = document.getElementById("inputHeight").value;
+  let gridWidth = document.getElementById("inputWidth").value;
 
-    // Create table rows
-    for (let tr = 0; tr < parseInt(gridHeight); tr++) {
-        const canvas = document.getElementById('pixelCanvas').insertRow(tr);
+  // Create table rows
+  for (let tr = 0; tr < parseInt(gridHeight); tr++) {
+    const canvas = document.getElementById("pixelCanvas").insertRow(tr);
 
-        //Create table columns
-        for (let td = 0; td < parseInt(gridWidth); td++) {
-
-            //Create a grid   
-            let box = canvas.insertCell(td);
-            //Assigned an id to each box of the grid
-            box.id = `${tr}${td}`;
-            box.onclick = function () { changeBackground(this.id); };
-        }
+    //Create table columns
+    for (let td = 0; td < parseInt(gridWidth); td++) {
+      //Create a grid
+      let box = canvas.insertCell(td);
+      //Assigned an id to each box of the grid
+      box.id = `${tr}${td}`;
+      box.onclick = function () {
+        changeBackground(this.id);
+      };
     }
-}
-
-
-/* Function to empty content of the table. This function will be used 
-* to reset the size of the grid. 
-*/
-const clearTable = () => {
-    let table = document.getElementById("pixelCanvas");
-    while (table.rows.length > 0) {
-        table.deleteRow(0);
-    }
-}
-
-/*
-* This function will change the background color of each grid
-* @param {string} id
-*/
-const changeBackground = (id) => {
-
-    // Target which element (td) should be changed its background
-    const el = document.getElementById(id);
-
-    // Switch between no background color and the user's selected color
-    const noBGColor = el.classList.toggle('noBGColor');
-
-    // Select color input
-    const drawingColor = document.getElementById('colorPicker').value;
-
-    /* Check if the background color exists. If not, set the background 
-    *  from the user's selected color.
-    */
-    noBGColor ? el.style.backgroundColor = drawingColor : el.style.backgroundColor = null;
-}
-
-/*
-* When the user click the submit button, it will reset the grid and the content of
-* each box will be emptied.
-* param {event} e 
-*/
-const formSubmit = (e) => {
-    e.preventDefault();
-    clearTable();
-    makeGrid();
+  }
 };
 
-// Select size input. 
-const form = document.getElementById('sizePicker');
+/* Function to empty content of the table. This function will be used
+ * to reset the size of the grid.
+ */
+const clearTable = () => {
+  const table = document.getElementById("pixelCanvas");
 
-/* 
-* When size is submitted by the user, call makeGrid()
-*/
+  while (table.rows.length > 0) {
+    //delete the first row in a table until it is empty
+    table.deleteRow(0);
+  }
+};
 
-form.addEventListener('submit', formSubmit);
+/*
+ * This function will change the background color of each grid
+ * @param {string} id
+ */
+const changeBackground = (id) => {
+  // Target which element (td) should be changed its background
+  const el = document.getElementById(id);
+
+  // Switch between no background color and the user's selected color
+  const noBGColor = el.classList.toggle("noBGColor");
+
+  // Select color input
+  const drawingColor = document.getElementById("colorPicker").value;
+
+  /* Check if the background color exists. If not, set the background
+   *  from the user's selected color.
+   */
+  noBGColor
+    ? (el.style.backgroundColor = drawingColor) //set the background color to the value of user's selected color
+    : (el.style.backgroundColor = null);
+};
+
+/*
+ * When the user click the submit button, it will reset the grid and the content of
+ * each box will be emptied.
+ * @param e - event
+ */
+const formSubmit = (e) => {
+  //  prevent from submitting the form
+  e.preventDefault();
+
+  clearTable();
+
+  //create a grid
+  makeGrid();
+};
+
+// Select size input.
+const form = document.getElementById("sizePicker");
+
+/*
+ * When size is submitted by the user, call makeGrid()
+ */
+
+form.addEventListener("submit", formSubmit);
